ca93265444dae1db261a75dc49652281c7967ab7
jacksoze <kamonyojack@gmail.com>
2023-01-05 15:52:29 +0300

    Make all nodes unvisited

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -5,3 +5,28 @@ class Cell
     @dist = dist
   end
 end
+
+def min_steps_to_target_pos(knight_pos, target_pos, N)
+
+  #all possible movements for the knight
+  dx = [2, 2, -2, -2, 1, 1, -1, -1]
+  dy = [1, -1, 1, -1, 2, -2, 2, -2]
+
+  queue = []
+
+  #push the starting position of the knight
+  #the distance should be 0
+  queue.append(cell(knight_pos[0], knight_pos[1], 0))
+
+  #mark all the nodes unvisited
+  visit = Array.new(N)
+
+  for i in 0..(N-1)
+    visit[i] = Array.new(N)
+    for j in 0..(N-1)
+      visit[i][j] = false
+    end
+  end
+
+  
+end
