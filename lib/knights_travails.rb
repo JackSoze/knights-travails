d6ee53c107d076e4aa76e5169c87216a5a0584d1
jacksoze <kamonyojack@gmail.com>
2023-01-09 12:00:31 +0300

    Create a min_steps_to target_pos def

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -2,8 +2,8 @@ class Cell
   attr_accessor :x, :y, :dist
 
   def initialize(x = 0, y = 0, dist = 0)
-    @x = x
-    @y = y
+    @x = x 
+    @y = y 
     @dist = dist
   end
 end
@@ -26,11 +26,11 @@ def min_steps_to_target_pos(knight_pos, target_pos, n)
   queue.append(Cell.new(knight_pos[0], knight_pos[1], 0))
 
   # make all the nodes unvisited
-  visited = Array.new(n)
+  visited = Array.new(n + 1)
 
-  for i in 0..(n - 1)
+  for i in 0..(n)
     visited[i] = Array.new(n)
-    for j in 0..(n - 1)
+    for j in 0..(n)
       visited[i][j] = false
     end
   end
@@ -54,10 +54,9 @@ def min_steps_to_target_pos(knight_pos, target_pos, n)
       x = t.x + dx[i]
       y = t.y + dy[i]
       
-      if (is_inside?(x, y, n) && !visited)
+      if (is_inside?(x, y, n) && !visited[x][y])
         visited[x][y] = true
-        queue.push(Cell.new(x, y, t.dist + 1))
-        puts "tata"
+        queue.push(Cell.new(x, y, t.dist + 1)) 
       end
     end
   end
@@ -66,4 +65,4 @@ end
 n = 30
 knight_pos = [1, 1]
 target_pos = [30, 30]
-puts min_steps_to_target_pos(knight_pos, target_pos, n).class
+puts min_steps_to_target_pos(knight_pos, target_pos, n)
